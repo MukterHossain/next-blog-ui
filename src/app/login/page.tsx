e6e79a56f79d17af6ebf10b9ed21c170fb0aced4
MukterHossain <mukter3075@gmail.com>
2025-09-24 06:43:44 +0600

    added register and login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import Login from "@/components/modules/Auth/Login";
+
+
+const LoginPage = () => {
+    return (
+        <div>
+           <Login></Login> 
+        </div>
+    );
+};
+
+export default LoginPage;
---
+++ src/app/register/page.tsx
@@ -0,0 +1,12 @@
+import Register from "@/components/modules/Auth/Register";
+
+
+const RegisterPage = () => {
+    return (
+        <div>
+            <Register></Register>
+        </div>
+    );
+};
+
+export default RegisterPage;
